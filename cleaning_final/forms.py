6d5060dffcca771e98e2ae857312fe075f0871fd
ramuir <65628879+ramuir@users.noreply.github.com>
2021-01-27 14:01:42 -0600

    finalizing model

--- cleaning_final/forms.py
+++ cleaning_final/forms.py
@@ -4,12 +4,13 @@ from wtforms.validators import DataRequired
 
 
 class DiabetesForm(FlaskForm):
-    Height = StringField('Height', validators = [DataRequired()])
-    BMI = StringField('BMI', validators = [DataRequired()])
     A1c = FloatField('A1c', validators = [DataRequired(float())])
-    HDL= StringField('HDL', validators = [DataRequired()])
-    Glucose = StringField('Glucose', validators = [DataRequired()])
-    Cholesterol = FloatField('Cholesterol', validators = [DataRequired(float())])
+    Glucose = FloatField('Glucose', validators = [DataRequired(float())])
+    Systolic = FloatField('bp.2s', validators = [DataRequired(float())])
+    Height = FloatField('Height', validators = [DataRequired(float())])
+    Age = FloatField('Age', validators = [DataRequired(float())])
+    WHratio = FloatField('bp.2s', validators = [DataRequired(float())])
+    
 
 
     submit = SubmitField('Get Checked')
