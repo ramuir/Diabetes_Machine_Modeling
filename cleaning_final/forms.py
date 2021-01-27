6758d99a319ad64b45ff7b5a304e88ea8aa604b1
ramuir <65628879+ramuir@users.noreply.github.com>
2021-01-27 01:00:58 -0600

    Fully working model

--- cleaning_final/forms.py
+++ cleaning_final/forms.py
@@ -1,16 +1,15 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, FloatField
 from wtforms.validators import DataRequired
 
 
 class DiabetesForm(FlaskForm):
-    A1c = StringField('A1c', validators = [DataRequired()])
-    Glucose = StringField('Glucose', validators = [DataRequired()])
-    BPSystolic = StringField('bp.2s', validators = [DataRequired()])
     Height = StringField('Height', validators = [DataRequired()])
-    Age = StringField('Age', validators = [DataRequired()])
-    WHratio = StringField('waist/hip ratio', validators = [DataRequired()])
-    # WHratio = StringField('waist/hip ratio', validators = [DataRequired()])
+    BMI = StringField('BMI', validators = [DataRequired()])
+    A1c = FloatField('A1c', validators = [DataRequired(float())])
+    HDL= StringField('HDL', validators = [DataRequired()])
+    Glucose = StringField('Glucose', validators = [DataRequired()])
+    Cholesterol = FloatField('Cholesterol', validators = [DataRequired(float())])
 
 
     submit = SubmitField('Get Checked')
