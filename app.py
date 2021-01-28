4f3841efac9499b48029dee45e4936eb7a586b50
ramuir <65628879+ramuir@users.noreply.github.com>
2021-01-28 10:24:33 -0600

    initial commit

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 from forms import DiabetesForm
 from flask import flash, redirect
+from flask_wtf import FlaskForm
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
